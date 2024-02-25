bd0777ea4294819bee7a98d38c8217abdcdc8d06
Tom Cowen <tomreescowen@gmail.com>
2024-02-25 12:32:16 +0000

    Run migrations and update admin.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-tomcow88-djangoblog-wm3m7u4x0xx.ws-eu108.gitpod.io",
                 '.herokuapp.com',]
